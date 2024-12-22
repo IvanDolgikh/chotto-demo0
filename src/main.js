420ddcd2c3887a645beaa71ed06088579e6661ac
IvanDolgikh <dolgiva0923@mail.ru>
2024-12-22 21:30:07 +0500

    first commit

---
+++ src/main.js
@@ -0,0 +1,12 @@
+import './style.css'
+import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+import '/node_modules/@mobilon-dev/chotto/dist/style.css';
+
+import App from './App.vue'
+
+const pinia = createPinia()
+
+
+createApp(App).use(pinia).mount('#app')
+
